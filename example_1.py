d11094126525d162ac87c130bebea07bc6163f67
Your Name <you@example.com>
2020-07-17 14:41:48 -0600

    Added example1.py

---
+++ example_1.py
@@ -0,0 +1,11 @@
+def print_name(name, surname):
+    """
+    This function takes two arguments name and surname from the user
+    Returns None
+
+    :param name: Name of the user
+    :param surname: Surname of the user
+
+    :return: None
+    """
+    return None
