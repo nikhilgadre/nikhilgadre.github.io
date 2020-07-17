c1deb141adb89e98eb248e0a537b54270662a55b
Your Name <you@example.com>
2020-07-17 16:28:01 -0600

    Trial 3

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -22,11 +22,11 @@ copyright = '2020, Nikhil Gadre'
 author = 'Nikhil Gadre'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
-import sphinx_rtd_theme
+import sphinx_rtd_theme, alabaster
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -34,8 +34,6 @@ import sphinx_rtd_theme
 extensions = [
     "sphinx_rtd_theme",
 ]
-
-html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,8 +50,13 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'alabaster'
+# html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+}
